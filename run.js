c0022be3b9cdee70983891678b4a93af02eb499c
Lesley <lesley.andrez@gmail.com>
2025-08-21 13:58:23 -0300

    Outro teste

--- run.js
+++ run.js
@@ -1,13 +1,1 @@
-alert('Oi, em 1 minuto vou apagar essa tela!');
-console.log('Oi de novo');
-var a = document.querySelector('.main-sidebar');
-if(a) {
-    a.style.outline = '1000px solid #ff00005e';
-    a.style.outlineOffset = '-1000px';
-}
-
-setTimeout(() => {
-    window.confirm("Tchau! Vou apagar tudo!");
-    document.body.innerHTML = '';    
-}, 1000 * 60);
-
+alert('Oi');
